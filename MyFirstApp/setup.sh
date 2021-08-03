4c7a0839f2e2feb504f16cfacd21159aad4bf9b4
Manisa Deep <87376522+Manisa-Deep@users.noreply.github.com>
2021-08-03 20:17:15 +0530

    Add files via upload

---
+++ MyFirstApp/setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email=\"manimohideep@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml 
+
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
